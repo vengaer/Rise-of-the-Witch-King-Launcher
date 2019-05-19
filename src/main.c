4879021353d00b0dc4fb3fc7585077563ab9f4e7
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2019-05-19 11:26:52 +0200

    Removed win gui

--- src/main.c
+++ src/main.c
@@ -1,76 +1,27 @@
 #include "config.h"
 #include "command.h"
 #include "fsys.h"
-#include "win_gui.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#if defined __CYGWIN__ || defined _WIN32
-#include <windows.h>
 
-char const g_szClassName[] = "Launcher";
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    WNDCLASSEX wc;
-    HWND hwnd;
-    MSG Msg;
-
-    wc.cbSize = sizeof(WNDCLASSEX);
-    wc.style = 0;
-    wc.lpfnWndProc = WndProc;
-    wc.cbClsExtra = 0;
-    wc.cbWndExtra = 0;
-    wc.hInstance = hInstance;
-    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wc.lpszMenuName = NULL;
-    wc.lpszClassName = g_szClassName;
-    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-
-    if(!RegisterClassEx(&wc)) {
-        MessageBox(NULL, "Window registration failed", "Error", MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
-
-    hwnd = CreateWindowEx(
-        WS_EX_CLIENTEDGE,
-        g_szClassName,
-        "Rise of the Witch-King Unofficial Launcher",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 600, 400,
-        NULL, NULL, hInstance, NULL);
-    if(hwnd == NULL) {
-        MessageBox(NULL, "Window creation failed", "Error", MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
-    
-    ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
-    
-    while(GetMessage(&Msg, NULL, 0, 0) > 0) {
-        TranslateMessage(&Msg);
-        DispatchMessage(&Msg);
-    }
-
-    return Msg.wParam;
-}
-
-#else
 int main(int argc, char** argv) {
     if(argc < 2) {
-        #if defined __CYGWIN__ || defined _WIN32
         if(!file_exists(CONFIG_FILE)) {
             /* gui setup */
         }
 
+        fprintf(stderr, "Usage: rotwkl [OPTION] [VERSION]\n\n");
+        fprintf(stderr, "    -r, --run           Run the given configuration\n");
+        fprintf(stderr, "    -s, --set           Specify what configuration to enable\n");
+        fprintf(stderr, "    -u, --update        Update config file for specified configuration\n");
+        fprintf(stderr, "    -h, --help          Display this help message\n");
+        fprintf(stderr, "Available configurations are:\n");
+        fprintf(stderr, "    rotwk\n    edain(*)\n    botta(*)\n");
+        fprintf(stderr, "* The mod to enable must be installed correctly\n");
         return 0;
-        #elif defined __linux__
-        fprintf(stderr, "GUI not available on Linux. Try rotwkl --help\n");
-        return 1;
-        #endif
     }
     
     if(argv[1][0] != '-') {
@@ -157,4 +108,3 @@ int main(int argc, char** argv) {
 
     return 0;
 }
-#endif
