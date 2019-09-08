1b68e741217d974648635a62ff788f869805e761
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2019-09-08 12:11:18 +0200

    Add command line progress bar

---
+++ src/progress_bar.c
@@ -0,0 +1,62 @@
+#include "progress_bar.h"
+#include <stdio.h>
+#include <string.h>
+
+void update_progress_buffer(struct progress_bar* pb);
+void format_indent(char* restrict indent, char const* restrict desc);
+
+void progress_bar_init(struct progress_bar* pb) {
+    int i;
+    for(i = 0; i < PROGRESS_BAR_LENGTH; i++)
+        pb->buffer[i] = '-';
+
+    pb->buffer[i] = '\0';
+}
+
+void progress_bar_display(struct progress_bar* pb, char const* desc) {
+    update_progress_buffer(pb);
+    char indent[PROGRESS_BAR_INDENT_BUF_LENGTH];
+    format_indent(indent, desc);
+
+    if(desc)
+        printf("\r%s%s[%s] : %u%%", desc, indent, pb->buffer, pb->progress);
+    else
+        printf("\r%s[%s] : %u%%", indent, pb->buffer, pb->progress);
+    fflush(stdout);
+}
+
+void progress_bar_finish(struct progress_bar* pb, char const* desc) {
+    pb->progress = 100;
+
+    update_progress_buffer(pb);
+
+    char indent[PROGRESS_BAR_INDENT_BUF_LENGTH];
+    format_indent(indent, desc);
+
+    if(desc)
+        printf("\r%s%s[%s] : %u%%\n", desc, indent, pb->buffer, pb->progress);
+    else
+        printf("\r%s[%s] : %u%%\n", indent, pb->buffer, pb->progress);
+    fflush(stdout);
+}
+
+void update_progress_buffer(struct progress_bar* pb) {
+    int i;
+    int const nhashes = (double)pb->progress / 100.0 * PROGRESS_BAR_LENGTH;
+
+    for(i = nhashes - 1; i >= 0; --i) {
+        if(pb->buffer[i] == '#')
+            break;
+
+        pb->buffer[i] = '#';
+    }
+}
+
+void format_indent(char* restrict indent, char const* restrict desc) {
+    unsigned i;
+    unsigned desc_len = desc ? strlen(desc) : 0;
+
+    for(i = 0; i < PROGRESS_BAR_INDENT - desc_len; i++)
+        indent[i] = ' ';
+    indent[i] = '\0';
+}
---
+++ src/progress_bar.h
@@ -0,0 +1,30 @@
+#ifndef PROGRESS_BAR_H
+#define PROGRESS_BAR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PROGRESS_BAR_INDENT 80
+#define PROGRESS_BAR_LENGTH 50
+#define PROGRESS_BAR_BUF_LENGTH 64
+#define PROGRESS_BAR_INDENT_BUF_LENGTH 128
+
+struct progress_bar {
+    unsigned progress;
+    char buffer[PROGRESS_BAR_BUF_LENGTH];
+};
+
+inline void progress_bar_set(struct progress_bar* pb, unsigned progress) {
+    pb->progress = progress;
+}
+
+void progress_bar_init(struct progress_bar* pb);
+void progress_bar_display(struct progress_bar* pb, char const* desc);
+void progress_bar_finish(struct progress_bar* pb, char const* desc);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
