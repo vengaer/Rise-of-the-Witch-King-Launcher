73c19fcb11bee9cd53fff540fb870ff48db712d1
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2019-09-08 10:36:37 +0200

    Use proper error reporting

--- src/fsys.c
+++ src/fsys.c
@@ -13,6 +13,8 @@
 #include <windows.h>
 #endif
 
+extern void(*display_error)(char const*);
+
 void toggle_big_files(struct big_file* enable, size_t enable_size, struct big_file* disable, size_t disable_size, char const* target_version, bool verify_active);
 void enable_big_file(struct big_file const* file, bool verify_active);
 void disable_big_file(struct big_file const* file, bool verify_active);
@@ -24,7 +26,7 @@ bool md5sum(char const* filename, char* checksum) {
     FILE* fp = fopen(filename, "rb");
 
     if(!fp) {
-        fprintf(stderr, "%s could not be opened for hashing\n", filename);
+        errorfmt("%s could not be opened for hashing\n", filename);
         return false;
     }
 
@@ -63,12 +65,12 @@ void set_active_configuration(char const* filename, char const* target_version, 
         bool const swap_successful = handle_swaps(swap, swap_size, target_version, use_version_dat);
 
         if(!swap_successful) {
-            fprintf(stderr, "Failed to swap .dat files, reverting\n");
+            display_error("Failed to swap .dat files, reverting\n");
             revert_changes(enable, enable_size, disable, disable_size, target_version);
         }
     }
     else
-        fprintf(stderr, "Errors encountered while reading game config, no changes will be made\n");
+        display_error("Errors encountered while reading game config, no changes will be made\n");
 
     free(enable);
     free(disable);
@@ -158,7 +160,7 @@ void enable_big_file(struct big_file const* file, bool verify_active) {
             char invalid[ENTRY_SIZE];
             strcpy(invalid, file->name);
             set_extension(invalid, INVALID_EXT);
-            fprintf(stderr, "Warning: File %s already exists. Will be moved to %s\n", file->name, invalid);
+            errorfmt("File %s already exists, will be moved to %s\n", file->name, invalid);
 
             rename(file->name, invalid);
         }
@@ -185,7 +187,7 @@ void disable_big_file(struct big_file const* file, bool verify_active) {
             char invalid[ENTRY_SIZE];
             strcpy(invalid, toggled);
             set_extension(invalid, INVALID_EXT);
-            fprintf(stderr, "Warning: File %s already exists. Will be moved to %s\n", toggled, invalid);
+            errorfmt("File %s already exists, will be moved to %s\n", toggled, invalid);
 
             rename(toggled, invalid);
         }
@@ -252,7 +254,7 @@ bool handle_swaps(struct dat_file const* swap, size_t swap_size, char const* tar
         md5sum(activate->disabled, hash);
 
         if(strcmp(activate->checksum, hash) != 0) {
-            fprintf(stderr, "No file matches the desired checksum\n");
+            display_error("No file matches the desired checksum\n");
             free(done);
             return false;
         }
@@ -284,5 +286,5 @@ void revert_changes(struct big_file* enable, size_t enable_size,
     if(file_exists(swp))
         rename(swp, toggled);
     else
-        fprintf(stderr, "Failed to restore .dat file\n");
+        display_error("Failed to restore .dat file\n");
 }
