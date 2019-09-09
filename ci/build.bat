6cc76d403dd1a8a09b8b753ce517b52e19894020
VilhelmEngstrom <vilhelm.engstrom@tuta.io>
2019-09-09 15:25:13 +0200

    Use separate build script

---
+++ ci/build.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+C:\msys64\msys2_shell.cmd -mingw64 -lc "C:/projects/rotwkl/ci/build_win C:/projects/rotwkl C:/Qt/5.13.0/mingw73_64"
+
+SET BUILD_RESULT=%ERRORLEVEL%
+
+type C:\projects\rotwkl\output.log
+del C:\projects\rotwkl\output.log
+
+EXIT /b %BUILD_RESULT%
